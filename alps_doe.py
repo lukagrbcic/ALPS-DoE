851b89ea36c016471796263a641efa5f3f7e576e
lukagrbcic <luka.grbcic@gmail.com>
2024-11-11 10:43:01 -0800

    added better samplers

--- alps_doe.py
+++ alps_doe.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.stats import qmc
 import matplotlib.pyplot as plt
-
+from scipy.spatial.distance import cdist
 
 class DOE:
     def __init__(self, n_samples, method, lb, ub, seed=None):
@@ -72,6 +72,113 @@ class DOE:
         
         return X
     
+    def greedyfp(self, scale=2):
+
+        M = self.n_samples * scale  
+        
+        candidates = np.random.uniform(self.lb, self.ub, size=(M, len(self.lb)))    
+        
+        
+        selected_indices = []
+        available_indices = list(range(M))
+    
+        first_idx = np.random.choice(available_indices)
+        selected_indices.append(first_idx)
+        available_indices.remove(first_idx)
+    
+        for _ in range(self.n_samples - 1):
+            selected_samples = candidates[selected_indices]  
+    
+            remaining_candidates = candidates[available_indices]  
+    
+            distances = cdist(remaining_candidates, selected_samples, metric='euclidean')
+    
+            min_distances = np.min(distances, axis=1)
+    
+            idx_in_remaining = np.argmax(min_distances)
+            idx_max_min_dist = available_indices[idx_in_remaining]
+    
+            selected_indices.append(idx_max_min_dist)
+            available_indices.remove(idx_max_min_dist)
+    
+        selected_samples = candidates[selected_indices]
+        
+        return selected_samples
+
+    def bc(self, scale=2):
+
+        M = self.n_samples * scale  
+        
+        candidates = np.random.uniform(self.lb, self.ub, size=(M, len(self.lb)))    
+        
+        
+        selected_indices = []
+        available_indices = list(range(M))
+    
+        first_idx = np.random.choice(available_indices)
+        selected_indices.append(first_idx)
+        available_indices.remove(first_idx)
+    
+        for _ in range(self.n_samples - 1):
+            selected_samples = candidates[selected_indices]  
+    
+            remaining_candidates = candidates[available_indices]  
+    
+            distances = cdist(remaining_candidates, selected_samples, metric='euclidean')
+    
+            min_distances = np.min(distances, axis=1)
+    
+            idx_in_remaining = np.argmax(min_distances)
+            idx_max_min_dist = available_indices[idx_in_remaining]
+    
+            selected_indices.append(idx_max_min_dist)
+            available_indices.remove(idx_max_min_dist)
+    
+        selected_samples = candidates[selected_indices]
+        
+        return selected_samples
+
+    def bc(self, scale=2, maxCand=50):
+        """
+        Generate N samples in a d-dimensional space that are far from each other
+        using the Best Candidate algorithm.
+    
+        Parameters:
+        - N: int, number of samples to generate.
+        - d: int, dimensionality of the space.
+        - scale: int, scale factor used to compute the number of candidates.
+        - maxCand: int, maximum number of candidate samples allowed.
+        - lower_bounds: array-like of shape (d,), lower bounds for each dimension.
+        - upper_bounds: array-like of shape (d,), upper bounds for each dimension.
+    
+        Returns:
+        - selected_samples: numpy array of shape (N, d), the selected samples.
+        """
+
+        
+        first_sample = np.random.uniform(self.lb, self.ub, size=(1, len(self.lb)))
+        selected_samples = [first_sample]  
+    
+        for i in range(2, self.n_samples + 1):
+
+            nCand = min(scale * i, maxCand)
+    
+            candidates = np.random.uniform(self.lb, self.ub, size=(nCand, len(self.lb)))
+
+            selected_array = np.vstack(selected_samples)
+    
+            distances = cdist(candidates, selected_array, metric='euclidean')  # Shape: (nCand, i-1)
+    
+            min_distances = np.min(distances, axis=1)
+    
+            idx_best_candidate = np.argmax(min_distances)
+            best_candidate_sample = candidates[idx_best_candidate]
+            
+            selected_samples.append(best_candidate_sample.reshape(1, len(self.lb)))
+    
+        selected_samples_array = np.vstack(selected_samples)
+        return selected_samples_array
+    
     
     def generate_samples(self):
         
@@ -95,16 +202,23 @@ class DOE:
         if self.method == 'random':
             
             return self.random()
-    
+        
+        if self.method == 'greedyFP':
+            
+            return self.greedyfp()
+        
+        if self.method == 'BC':
+            
+            return self.bc()
     
 
-# method = 'lhs'   
-# n_samples = 1000
-# lb = np.array([0.2, 10, 15])
-# ub = np.array([1.3, 700, 28])     
+method = 'BC'   
+n_samples = 1000
+lb = np.array([0.2, 10, 15])
+ub = np.array([1.3, 700, 28])     
 
-# experiment = DOE(n_samples, method, lb, ub)
-# print (experiment.generate_samples())
+experiment = DOE(n_samples, method, lb, ub)
+samples = experiment.generate_samples()
         
         
     
