84ed86a948fcfceb97acf7fd0a015be7200705d3
lukagrbcic <luka.grbcic@gmail.com>
2026-07-07 13:56:43 -0700

    conversion script

--- jpeg_to_bitmap.py
+++ jpeg_to_bitmap.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PIL import Image
 
-img = Image.open("unitCell.jpeg").convert("L")   # load as grayscale
+img = Image.open("uc2.png").convert("L")   # load as grayscale
 arr = np.array(img)
 print(arr.min(), arr.max())
 print("fraction white:", (arr == 255).mean())
